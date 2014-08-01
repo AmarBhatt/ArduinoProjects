9e19e478ba16624e08d5da5f9235365371ba82fe
Amar Bhatt <bhatt.amar.a@gmail.com>
2014-07-31 23:28:12 -0400

    Sensor Project 8
    Photoresistor code updates

--- SensorProject8_AdjustingNightlight/SensorProject8_AdjustingNightlight.ino
+++ SensorProject8_AdjustingNightlight/SensorProject8_AdjustingNightlight.ino
@@ -7,31 +7,25 @@
 * Author: Amar Bhatt
 */
 
-//NOTE: When there is light sensor is LOW, when it is not it is HIGH
+//NOTE: When there is light sensor is LOW, when there is not it is HIGH
 //      Range: 0-1023
 
 
 //Define Pins used
-const int sensorPin = A0; //analog
-const int led1 = 3; //PWM pin for brightness control
-const int led2 = 5; //PWN pin for brightness control
-const int led3 = 6; //PWM pin for brightness control
+const int sensorPin = A0; //Photoresistor sensor
+const int ledPin = 6; //PWM pin for LEDs (connected in parallel)
 
 void setup () {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin (9600);
 }
 void loop () {
-  int value = analogRead (sensorPin);
+  int value = analogRead (sensorPin); //Get the brightness value as determined by the sensor
   //Serial.println (value, DEC);
-  value = map(value, 150, 900, 0, 255); //map the low and the high
+  value = map(value, 400, 900, 0, 255); //map the low and the high
   value = constrain(value, 0, 255); //constrain any outliers
-  //Serial.println(value);
-  analogWrite(led1, value);
-  analogWrite(led2, value);
-  analogWrite(led3, value);
+  Serial.println(value);
+  analogWrite(ledPin, value);
   delay (50);
 }
 
